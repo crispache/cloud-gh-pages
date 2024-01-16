dbfe5452e702c6815a67f910544c3da6ce09edda
crispache <cristinaglezpache06@gmail.com>
2024-01-16 12:23:52 +0000

    Add project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+/* import ReactDOM from 'react-dom'; */
+import { createRoot } from 'react-dom/client';
+
+import App from './app';
+
+const t = createRoot(document.getElementById('root'))
+t.render(<App />)
+
+/* ReactDOM.render(<App />, document.getElementById('root')); */
